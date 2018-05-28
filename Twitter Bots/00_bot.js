8afe796b96c82ca9aefe86f234a9b5e3bf240195
Tanawat Boonmak <latte.book@gmail.com>
2018-05-28 15:47:21 +0700

    Add files via upload

---
+++ Twitter Bots/00_bot.js
@@ -0,0 +1,13 @@
+{
+	"origin": ["#skirt_promo#", "#skirt_promo#", "#skirt_promo#", "#skirt_tw5#", "#skirt_tw6#", "#skirt_des1#", "#shirt_des1#", "#shirt_tw2#", "#shirt_tw3#"],
+	"skirt_tw":["กระโปรงเทนนิสราคาถูก\n ✔ 250 บาททุกตัว\n ✔ เนื้อผ้า : ฮานาโกะ มีกางเกงซับใน\n ✔ADDLINE @ydb8852w \n\\#กระโปรงเทนนิส \\#เสื้อผ้าเกาหลี \\#เสื้อผ้าพร้อมส่ง  \\#เสื้อผ้าแฟชั่น #skirt_api_promo#"],
+	"skirt_api_promo":["{svg <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"800\" height=\"600\"><image width=\"800\" height=\"600\" xlink:href=\"#skirt_ref_promo#\" /></svg>}"],
+	"skirt_ref_promo":["https://pbs.twimg.com/media/DaBlnfwVQAAtHdV.jpg:large"],
+
+  "shirt_tw2":["เสื้อยืดแนวเกาหลีทรงโอเวอร์ไซส์ 🌈 \n✔170฿ \n✔ ลทบ. + 30 / ems + 50 ( ตัวต่อไป +10 ) \n✔ ไซส์ : อก 44 นิ้ว ยาว 25 นิ้ว \n✔ เนื้อผ้า : คอตตอน 100% นุ่มมาก ไม่บาง \n✔ สั่งซื้อ ทักแชทอินบ๊อกได้เลยจ้า หรือแอดไลน์ @ydb8852w [ มี@ด้วยนะคะ ] \n\\#เสื้อผ้าเกาหลี \\#เสื้อยืดแนวเกาหลี \\#เสื้อทรงโอเวอร์ไซส์ #shirt_api2#"],
+	"shirt_api2":["{svg <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"800\" height=\"600\"><image width=\"800\" height=\"600\" xlink:href=\"#shirt_ref2#\" /></svg>}"],
+	"shirt_ref2":["https://pbs.twimg.com/media/DWJct-PU8AA_6eE.jpg:large"],
+
+	"shirt_des1":["เสื้อยืดแนวเกาหลีทรงโอเวอร์ไซส์ 🌈 \n✔170฿ \n✔ ลทบ. + 30 / ems + 50 ( ตัวต่อไป +10 ) \n✔ ไซส์ : อก 44 นิ้ว ยาว 25 นิ้ว \n✔ เนื้อผ้า : คอตตอน 100% \n สนใจดูในโปรไฟล์ก่อนได้ค่ะ \n\\#เสื้อผ้าเกาหลี \\#เสื้อยืดแนวเกาหลี \\#เสื้อทรงโอเวอร์ไซส์"]
+
+}
